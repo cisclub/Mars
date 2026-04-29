98f8fc7150250d978fd6dbb31a73de07aeed9d25
ali amin <ali.ntg3@gmail.com>
2026-04-29 11:28:46 +0400

    updates
    optimize
    add delete option
    include .h and .m files

--- Mars.fish
+++ Mars.fish
@@ -2,42 +2,47 @@
 # Script to find unused xib and storyboard files in a project
 
 
-set fpaths (grep -r --include="*.xib" --include="*.storyboard" -l "")
-set -l all
-for fpath in $fpaths
-	set nm (basename $fpath)
-    set -a all $nm
+set searchPath $argv[1]
+if count $argv[2] > /dev/null
+    set limit $argv[2]
 end
 
+set fpaths (grep -r --include="*.xib" --include="*.storyboard" -l "" $searchPath)
 set -l toRemove
-set count 1
-for withExt in $all
-    set count (math $count + 1)
-    # if test $count -eq 200
-    #     break
-    # end
-    set nib (path change-extension '' $withExt)
-    printf "%s: " $withExt
-    set filesContainXib (grep -rl --include="*.xib" --include="*.storyboard" --include="*.swift" $nib)
-    set -l filesThatUse
-    for file in $filesContainXib
-        set fileNameWithExt (basename $file)
-        set fileName (path change-extension '' $fileNameWithExt)
-        set isNotSameFile $fileName != $nib
-        if test $isNotSameFile
-            set -a filesThatUse $fileName
-        end    
+set count 0
+for fpath in $fpaths
+    set name (basename $fpath)
+    set nib (path change-extension '' $name)
+    printf "\n%s: " $fpath
+    set swiftFile $nib.swift
+    set nibFile $nib.xib
+    set sbFile $nib.storyboard
+    set objmFile $nib.m
+    set objhFile $nib.h
+    if grep -r --silent --include="*.m" --include="*.h" --include="*.xib" --include="*.storyboard" --include="*.swift" --exclude=$swiftFile --exclude=$nibFile --exclude=$sbFile --exclude=$objmFile --exclude=$objhFile $nib $searchPath
+        printf "\tused"
+    else
+        set -a toRemove $fpath
+        printf "\tnot used"
     end
-    set isEmpty (count $filesThatUse) -eq 0
-    if test $isEmpty
-        set -a toRemove $nib
-        printf "\tnot used\n"
-    else 
-        printf "\tused\n"
+
+    if test $limit
+        set count (math $count + 1)
+        if test $count -eq $limit
+            break
+        end
     end
 end
 
 printf "\n\nSummary\n-------\n"
 printf "Unused files:\n\n"
 printf "%s\\n" $toRemove
-printf "\nNumber of unsed files:\t(%d)\t\tTotal size:\t\n" $(count $toRemove)
+printf "\nNumber of unused files:\t(%d)\t\tTotal size:\t\n" $(count $toRemove)
+
+read -P "Do you want to delete unused files? (type \"YES DELETE\" all in capital to confirm. move unreversable): " ans
+if test $ans = "YES DELETE"
+    for path in $toRemove
+        rm $path
+    end
+    printf "All unused files deleted.\n"
+end
